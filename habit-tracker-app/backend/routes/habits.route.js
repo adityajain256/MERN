3b7961afa75fb2b8ecc992cd54fe4dbcf9e44a23
“adityajain256” <jainaditya25522@gmail.com>
2025-09-13 16:04:45 +0530

    Update habits.route.js

--- habit-tracker-app/backend/routes/habits.route.js
+++ habit-tracker-app/backend/routes/habits.route.js
@@ -3,6 +3,9 @@ import express from 'express';
 const habitRouter = express.Router();
 
 habitRouter.get("/habit");
+habitRouter.post("/habit");
+habitRouter.patch("/habit/:id");
+habitRouter.delete("/habit/:id");
 
 
 export default habitRouter;
